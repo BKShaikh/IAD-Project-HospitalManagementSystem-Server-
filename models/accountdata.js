144c501c963d3f760c6eabee0fe118046fcf1600
Bilal Shaikh <bilal_shaikh@live.com>
2017-04-29 15:09:23 +0500

    accountdata is edited

--- models/accountdata.js
+++ models/accountdata.js
@@ -2,7 +2,8 @@
 module.exports = function(sequelize, DataTypes) {
   var accountdata = sequelize.define('accountdata', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    roleId:DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
